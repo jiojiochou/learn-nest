d174aff956dab1c11fb7960ad52f8866e5879882
codercsx <1694148878@qq.com>
2024-06-12 09:17:39 +0800

    feat: use-session

--- all-decorator/src/main.ts
+++ all-decorator/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'session?',
+      cookie: { maxAge: 100000 },
+    }),
+  );
   await app.listen(9527);
 }
 bootstrap();
