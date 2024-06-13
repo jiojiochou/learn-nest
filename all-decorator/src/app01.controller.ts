425fe49db88f31138c296b80a7c9e0b7adfe226d
codercsx <1694148878@qq.com>
2024-06-13 17:37:38 +0800

    feat: 设置controller的host

--- all-decorator/src/app01.controller.ts
+++ all-decorator/src/app01.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('app01')
-export class App01Controller {}
+@Controller({ host: ':host.0.0.1', path: 'aaa' })
+export class App01Controller {
+  @Get()
+  hello1() {
+    return 'hello app01.controller /';
+  }
+  @Get('bbb')
+  hello2() {
+    return 'hello app01.controller /bbb';
+  }
+}
