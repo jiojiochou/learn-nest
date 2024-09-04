29e083747c7ce73e6b21bf739d2c211ce01e6d95
codercsx <83553175@qq.com>
2024-09-04 17:14:56 +0800

    feat: 初始化nest-upload

--- multer-test/index.js
+++ multer-test/index.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const multer = require("multer");
-const cors = require("cors");
+const express = require('express');
+const multer  = require('multer');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
-const upload = multer({dest: 'uploads/'});
+const upload = multer({ dest: 'uploads/' });
 
-app.post("/aaa", upload.single("aaa"), function(req, res, next) {
-  console.log("req.file", req.file);
-  console.log("req.body", req.body);
-})
+app.post('/aaa', upload.single('aaa'), function (req, res, next) {
+  console.log('req.file', req.file);
+  console.log('req.body', req.body);
+});
 
-app.listen(3333);
+app.post('/bbb', upload.array('bbb', 2), function (req, res, next) {
+  console.log('req.file', req.files);
+  console.log('req.body', req.body);
+});
+
+app.listen(9527, function() {
+  console.log("Server is running on port 9527");
+});
